c09228cf3e3b4341afaff017256140ef679bae28
Matt <matt.uiarena@gmail.com>
2022-05-01 14:43:35 +0530

    indexes

---
+++ map.js
@@ -0,0 +1,17 @@
+$(function () {
+  var overlay = $('<div id="overlay"></div>');
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(".popup-onload").show();
+  $(".close").click(function () {
+    $(".popup-onload").hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+  });
+
+  $(".x").click(function () {
+    $(".popup").hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+  });
+});
